a9653a6ea79d9cbfad1d0ab453042dad3b16e586
Kaionji-S <violet210@hotmail.com>
2026-05-15 00:07:35 +0800

    update

---
+++ Test_Vue3_20250410/Scripts/Site/code-viewer.js
@@ -0,0 +1,62 @@
+﻿const CodeViewer = {
+    props: ['code', 'lang'],
+
+    template: `
+    <pre>
+      <code ref="codeEl" :class="'language-' + lang"></code>
+    </pre>
+  `,
+
+    mounted() {
+        this.renderHighlight();
+    },
+
+    updated() {
+        this.renderHighlight();
+    },
+
+    methods: {
+
+        renderHighlight() {
+
+            const el = this.$refs.codeEl;
+
+            el.textContent = this.code;
+
+            delete el.dataset.highlighted;
+
+            hljs.highlightElement(el);
+        }
+    }
+};
+
+
+<code-viewer
+    lang="csharp"
+  : code="item.code">
+</code-viewer>
+
+
+.code - block {
+    width: 100 %;
+    max - width: 100 %;
+    overflow - x: auto;
+    min - width: 0;
+}
+
+.code - block pre {
+    margin: 0;
+    overflow - x: auto;
+}
+
+.code - block code {
+    white - space: pre;
+}
+
+<div class="code-block">
+    <pre>
+        <code class="language-csharp">
+            {{ code }}
+        </code>
+    </pre>
+</div>
